4dcd97aad6afb5285e2fa95abb63e15249804c9e
Yang Xuetao (uid03205) <xuetao.yang@desaysv.com>
2026-03-25 14:47:57 +0800

    调整预设食物

--- netlify/functions/_lib/presets.js
+++ netlify/functions/_lib/presets.js
@@ -1,22 +1,10 @@
 export const FOOD_PRESETS = [
-  { food_name: "鸡蛋", kcal_per_100g: 144 },
   { food_name: "米饭", kcal_per_100g: 130 },
-  { food_name: "鸡胸肉", kcal_per_100g: 133 },
-  { food_name: "虾仁", unit: "只", kcal_per_unit: 6 },
-  { food_name: "牛肉", kcal_per_100g: 170 },
   { food_name: "豆腐", kcal_per_100g: 81 },
-  { food_name: "玉米", kcal_per_100g: 106 },
   { food_name: "面条", kcal_per_100g: 137 },
-  { food_name: "牛奶", kcal_per_100g: 54 },
-  { food_name: "包子", kcal_per_100g: 230 },
   { food_name: "西蓝花", kcal_per_100g: 34 },
-  { food_name: "苹果", kcal_per_100g: 53 },
   { food_name: "香蕉", kcal_per_100g: 93 },
-  { food_name: "番茄", kcal_per_100g: 18 },
-  { food_name: "红薯", kcal_per_100g: 86, unit: "g" },
-  { food_name: "麦当劳1+1", unit: "份", kcal_per_unit: 586 },
-  { food_name: "蔬菜沙拉", kcal_per_100g: 30, unit: "g" },
-  { food_name: "美式", kcal_per_100g: 2 }
+  { food_name: "坚果", kcal_per_100g: 600, unit: "g" }
 ];
 
 export const EXERCISE_PRESETS = [
